d95df4072f181a6e49cb7d416a740ee58190e06c
m-ogore <ogoremarv@gmail.com>
2023-10-16 06:31:06 +0200

    modified:   math/multivariate_prob/0-mean_cov.py

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -20,18 +20,8 @@ def mean_cov(X): #X is a numpy.ndarray of shape (n, d) containing the data set:
     #Calculate the mean for each dimension:
     mean = np.mean(X, axis=0, keepdims=True)
     #Calculate the covariance matrix:
-    cov=X-mean/(n-1)
+    cov=(X-mean)/(n-1)
     #for x in enumerate(X):
      #    for mean in means:
       #       cov.append(sum(x-mean)/(n-1))
     return mean,cov
-'''
-    
-    #mean is a numpy.ndarray of shape (1, d) containing the mean of the data set
-    meam = np.mean(X, axis=0) #Calculate the mean for each dimension
-    #cov is a numpy.ndarray of shape (d, d) containing the covariance matrix of the data set
-    cov = np.cov(X, rowvar=False) #Calculate the covariance matrix
-
-    return mean, cov
-         
-'''
