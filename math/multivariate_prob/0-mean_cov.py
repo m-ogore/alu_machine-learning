b3024b3a4ff7dc0b010665a376ba5294cf9f6f4d
m-ogore <ogoremarv@gmail.com>
2023-10-16 06:41:38 +0200

    modified:   math/multivariate_prob/__pycache__/0-mean_cov.cpython-310.pyc

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -6,13 +6,13 @@ def mean_cov(X): #X is a numpy.ndarray of shape (n, d) containing the data set:
     X=np.array(X)
     #n is the number of data points
     #d is the number of dimensions in each data point
-    #n = len(X)
-    #d = len(X[0])
+    n = len(X)
+    d = len(X[0])
 
-    (n,d)=X.shape
+    #(n,d)=X.shape
 
     #If X is not a 2D numpy.ndarray, raise a TypeError with the message X must be a 2D numpy.ndarray
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    if X.ndim != 2:
          raise TypeError("X must be a 2D numpy.ndarray")
     #If n is less than 2, raise a ValueError with the message X must contain multiple data points
     if n<2:
