923055eabd72b1b87faa3b3f55b1fd3c36602c72
m-ogore <ogoremarv@gmail.com>
2023-11-09 11:32:24 +0200

    checkers for all

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
+'''
+You are conducting a study on a revolutionary cancer drug and are looking to find the probability that a patient who takes this drug will develop severe side effects. During your trials, n patients take the drug and x patients develop severe side effects. You can assume that x follows a binomial distribution.
+
+Write a function def likelihood(x, n, P): that calculates the likelihood of obtaining this data given various hypothetical probabilities of developing severe side effects:
+
+x is the number of patients that develop severe side effects
+n is the total number of patients observed
+P is a 1D numpy.ndarray containing the various hypothetical probabilities of developing severe side effects
+If n is not a positive integer, raise a ValueError with the message n must be a positive integer
+If x is not an integer that is greater than or equal to 0, raise a ValueError with the message x must be an integer that is greater than or equal to 0
+If x is greater than n, raise a ValueError with the message x cannot be greater than n
+If P is not a 1D numpy.ndarray, raise a TypeError with the message P must be a 1D numpy.ndarray
+If any value in P is not in the range [0, 1], raise a ValueError with the message All values in P must be in the range [0, 1]
+Returns: a 1D numpy.ndarray containing the likelihood of obtaining the data, x and n, for each probability in P, respectively
+
+
+'''
+
 import numpy as np
 
 def likelihood(x, n, P):
     # n must be a positive integer
-    if not isinstance(n, int) or n < 0:
+    if not isinstance(n, int) or n <= 0:
         raise ValueError('n must be a positive integer')
 
     # x must be an integer that is greater than or equal to 0
@@ -15,9 +33,13 @@ def likelihood(x, n, P):
     if x > n:
         raise ValueError('x cannot be greater than n')
 
-    # If P is not a 1D numpy.ndarray, raise a TypeError with the message "P must be a 1D numpy.ndarray"
-    if P.ndim != 1:
+    # Check if P is a 1D numpy.ndarray
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError('P must be a 1D numpy.ndarray')
+
+    # Check if values in P are in the range [0, 1]
+    if not all(0 <= val <= 1 for val in P):
+        raise ValueError('All values in P must be in the range [0, 1]')
 
     # Initialize an empty array to store the likelihoods
     likelihoods = np.zeros(P.shape)
