6877bb75ed43ceec8560b75499ca5595f0ea35b5
m-ogore <ogoremarv@gmail.com>
2024-03-12 09:28:16 +0200

    sql-metal-bands lifespan

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -9,7 +9,9 @@
 --You should use attributes formed and split for computing the lifespan
 --Your script can be executed on any database
 
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+-- SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+
+
+SELECT band_name, IF(split IS NULL, 2020, split) - formed AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC
