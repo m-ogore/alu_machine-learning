5786efa748f56de25c4230e4d3ec7b8700080434
m-ogore <ogoremarv@gmail.com>
2024-03-13 15:44:49 +0200

    api data

---
+++ pipeline/apis/4-rocket_frequency.py
@@ -0,0 +1,27 @@
+import requests
+
+def get_launch_data():
+    response = requests.get("https://api.spacexdata.com/v4/launches")
+    data = response.json()
+    
+    return data
+
+def count_launches_per_rocket(launches):
+    rocket_count = {}
+    for launch in launches:
+        rocket_name = launch['rocket']
+        if rocket_name in rocket_count:
+            rocket_count[rocket_name] += 1
+        else:
+            rocket_count[rocket_name] = 1
+    return rocket_count
+
+def main():
+    launches = get_launch_data()
+    rocket_count = count_launches_per_rocket(launches)
+    sorted_rocket_count = sorted(rocket_count.items(), key=lambda x: (-x[1], x[0]))
+    for rocket, count in sorted_rocket_count:
+        print(f"{rocket}: {count}")
+
+if __name__ == '__main__':
+    main()
--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -14,25 +14,16 @@
 
 -- USE metal_bands;
 -- SELECT band_name, IF(split IS NULL, 2020, split) - IF(formed IS NULL,0,formed) AS lifespan FROM me
---tal_bands ORDER BY lifespan DESC;
+-- tal_bands ORDER BY lifespan DESC;
 
 
--- Create a view to compute the lifespan of each band
-CREATE OR REPLACE VIEW band_lifespan AS
 SELECT
     band_name,
     IFNULL(split, YEAR('2020-01-01')) - formed AS lifespan
 FROM
     metal_bands;
-
--- List all bands with Glam rock as their main style, ranked by their longevity
-SELECT
-    band_name,
-    lifespan
-FROM
-    band_lifespan
-WHERE
-    band_name IN ('Alice Cooper', 'Mötley Crüe', 'Marilyn Manson', 'The 69 Eyes', 'Hardcore Superstar', 'Nasty Idols', 'Hanoi Rocks')
 ORDER BY
     lifespan DESC;
+
+
 
