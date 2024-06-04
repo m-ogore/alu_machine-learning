d56cc7c1cdb112545cb41bf4f70e5aeb953b3079
m-ogore <ogoremarv@gmail.com>
2024-06-04 11:00:56 +0200

    pmf: Poisson func

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,48 +1,44 @@
-import numpy as np
-''' 
-    Create a class Poisson that represents a poisson distribution:
+#!./.intranet/bin/python
 
-    Class contructor def __init__(self, data=None, lambtha=1.):
-    data is a list of the data to be used to estimate the distribution
-    lambtha is the expected number of occurences in a given time frame
-    Sets the instance attribute lambtha
-    Saves lambtha as a float
-    If data is not given, (i.e. None (be careful: not data has not the same result as data is None)):
-    Use the given lambtha
-    If lambtha is not a positive value or equals to 0, raise a ValueError with the message lambtha must be a positive value
-    If data is given:
-    Calculate the lambtha of data
-    If data is not a list, raise a TypeError with the message data must be a list
-    If data does not contain at least two data points, raise a ValueError with the message data must contain multiple values
-'''
+import numpy as np
 
 class Poisson:
 
-    def __init__(self, data = None, lambtha = 1.):
+    def __init__(self, data=None, lambtha=1.):
         self.data = data
-        
-        if self.data is None:
-            
-            if lambtha<=0:
-                raise ValueError('lambtha must be a positive value')
+
+        self.lambtha = float(lambtha)
+
+        if data is None:
             self.lambtha = float(lambtha)
+
+            if self.lambtha <= 0:
+                 raise ValueError('lambtha must be a positive value')
         else:
-            #calculate the lambtha of the data 
-
             if not isinstance(data,list):
-                raise  TypeError(' data must be a list')
-        
-            if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-        
-            self.lambtha = sum(data)/len(data)
+                raise TypeError('data must be a list')
+            if len(self.data) < 2:
+                raise ValueError('data must contain multiple values')
+            #Calculate the lambtha of data
 
+            self.lambtha = np.sum(data)/len(data)
 
-
+    def pmf(self, k): 
+        self.k = int(k)       
+        if self.k < 0:
+             
+            return 0
+        else:
+            
+            #k is out of range
+    
+            return (np.exp(-self.lambtha)*self.lambtha**self.k)/np.math.factorial(self.k)
 np.random.seed(0)
 data = np.random.poisson(5., 100).tolist()
 p1 = Poisson(data)
-print('Lambtha:', p1.lambtha)
+print('P(9):', p1.pmf(9))
 
 p2 = Poisson(lambtha=5)
-print('Lambtha:', p2.lambtha)
+print('P(9):', p2.pmf(9))
+
+
